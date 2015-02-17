d34a280434bab30c82ab46ce22fdae79fe7acb46
Max Gordon <gordonmaxc@gmail.com>
2015-02-17 10:13:15 -0800

    filter single char game names

--- BoardGameMatcher/app/routes/games.js
+++ BoardGameMatcher/app/routes/games.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() { 
-    return this.store.find('game');
+    this.store.find('game')
+    return this.store.filter('game', function  (game) {
+      return game.get("title").length > 1;
+    }) 
+    // return this.store.find('game');
   }
 });
